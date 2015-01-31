1eba97dea8671fe85a64437858935ba53e5b64ac
Robin Vivant <robin.vivant@gmail.com>
2015-01-31 21:18:28 +0100

    recherche de fichiers

--- client/components/room/files/files.js
+++ client/components/room/files/files.js
@@ -3,11 +3,20 @@
 Template.files.helpers({
     files: function(){
         return Files.find();
+    },
+    showFile: function(){
+        var filter = Session.get('fileFilter');
+        if(!filter)
+            return true;
+        return this.name.match(new RegExp(filter, 'i'));
     }
 });
 
 Template.files.events({
 
+    'keyup .search-file > input': function(e, tmpl){
+        Session.set('fileFilter', e.currentTarget.value.trim());
+    }
 });
 
 Template.files.created = function(){
