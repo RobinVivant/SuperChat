082c220f78743bdec98f1036bae4f5f0ea18ff9d
Thomas Montana <thmsmontana@gmail.com>
2015-02-11 16:15:25 +0100

    Completed room selector UI
    It’s awesome.

--- client/pages/home/home.js
+++ client/pages/home/home.js
@@ -1,3 +1,5 @@
+var roomFieldValue = new ReactiveVar();
+var isRoomOwner = new ReactiveVar(false);
 
 Template.home.helpers({
     userName: function(){
@@ -7,13 +9,36 @@ Template.home.helpers({
         }
     },
     rooms: function(){
-        return Rooms.find({}, {
+        var rooms = Rooms.find({}, {
             sort: {name: 1}
         }).fetch();
+        return rooms;
     },
     roomName: function(){
         return Session.get('roomName');
+    },
+    getRoomFieldValue: function () {
+        return roomFieldValue.get();
+    },
+    getIsRoomOwner: function () {
+        return isRoomOwner.get();
     }
+    ,createDisabledOrNot: function () {
+        var val = roomFieldValue.get();
+        if (val == '' || Rooms.find({'name': val}).count()) {
+            return 'disabled';
+        } 
+    },
+    renameDisabledOrNot: function () {
+        if (!Session.get('roomId') || !isRoomOwner.get() || roomFieldValue.get().length == 0 || roomFieldValue.get() == Rooms.findOne({_id: Session.get('roomId')}).name) {
+            return 'disabled';
+        }
+    }
+});
+
+Template.registerHelper('roomHiddenOrNot', function (name) {
+    if (roomFieldValue.get().length > 0 && name.indexOf(roomFieldValue.get()) == -1)
+        return 'hidden';
 });
 
 var usernameTimeout, roomTimeout;
@@ -28,32 +53,38 @@ Template.home.events({
             Meteor.call('updateUserName', Session.get('roomId'), localStorage.token, e.currentTarget.value.trim());
         }, 200);
     },
-    'keyup .room-name': function(e, tmpl){
-        if( e.currentTarget.value.trim().length == 0 || e.keyCode != 8 && e.keyCode != 46 && (e.keyCode < 65 || e.keyCode > 90))
-            return;
-        clearTimeout(roomTimeout);
-        roomTimeout = setTimeout(function() {
-            Meteor.call('updateRoomName', Session.get('roomId'), localStorage.token, e.currentTarget.value.trim());
-        }, 200);
+    'click #renameRoom': function(e, tmpl){
+        console.log(roomFieldValue.get());
+        Meteor.call('updateRoomName', Session.get('roomId'), localStorage.token, roomFieldValue.get());
     },
-    'click .room-destroy': function(e, tmpl){
-        Meteor.call('removeRoom', Session.get('roomId'), localStorage.token, function(error, result){
-
+    'click #createRoom': function(e, tmpl) {
+        Rooms.insert({name: roomFieldValue.get(), owner: localStorage.token}, function (error, newRoomId) {
+            Session.set('roomId', newRoomId);
         });
     },
+    'input #roomName, change #roomName': function(e, tmpl) {
+        var val = $('#roomName').val();
+        roomFieldValue.set(val);
+    },
+    'click #destroyRoom': function(e, tmpl){
+        Meteor.call('removeRoom', Session.get('roomId'), localStorage.token);
+        roomFieldValue.set('');
+    },
     'focus #roomSetting>input': function (e, tmpl) {
-        console.log(e);
         $('#roomSetting').addClass('active');
     },
     'click #roomSetting>*': function (e, tmpl) {
         e.stopImmediatePropagation();
     },
-    'click #roomList > li': function(e, tmpl) {
+    'click #roomList > li.room': function(e, tmpl) {
         Session.set('roomId', this._id);
+        Session.set('roomName', this.name);
+        $('#roomName').val(this.name);
     },
     'click': function (e, tmpl) {
         if (e.currentTarget != document.getElementById('roomSetting')) {
             $('#roomSetting').removeClass('active');
+            $('#roomName').val(Session.get('roomName'));
         }
     }
 });
@@ -70,6 +101,12 @@ Template.home.created = function(){
         }
         $(window).on('resize', doIt);
         doIt();
+    });
+
+    Tracker.autorun(function () {
+        Meteor.call('isRoomOwner', Session.get('roomId'), localStorage.token, function (error, result) {
+            isRoomOwner.set(result);
+        });
     });
 
 
@@ -148,6 +185,7 @@ Template.home.created = function(){
             Session.set('roomName', null);
         }else{
             Session.set('roomName', room.name);
+            roomFieldValue.set(room.name);
         }
         window.history.replaceState(Session.get('roomName'), Session.get('roomName'), '/'+(Session.get('roomId')||''));
     });
